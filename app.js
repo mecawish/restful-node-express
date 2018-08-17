4c078c2c976d7508e5894e07d95ef1ead912a5f8
Meliza Caguimbal <meca@wpdomain.local>
2018-08-17 23:20:21 +0800

    wire up to mongodb and mongoose

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
-var express = require('express');
+var express = require('express'),
+  mongoose = require('mongoose');
+
+var db = mongoose.connect('mongodb://localhost/bookAPI');
+
+var Book = require('./models/bookModel');
 
 var app = express();
 
 var port = process.env.PORT || 3500;
 
+var bookRouter = express.Router();
+
+bookRouter.route('/Books').get(function(req, res) {
+  Book.find(function(err, books) {
+    if (err) res.status(500).send(err);
+    else res.json(books);
+  });
+});
+
+app.use('/api', bookRouter);
+
 app.get('/', function(req, res) {
-    res.send('Welcome to my API!');
+  res.send('Welcome to my API!');
 });
 
 app.listen(port, function() {
-    console.log('Gulp is running my app on PORT: ' + port);
+  console.log('Gulp is running my app on PORT: ' + port);
 });
