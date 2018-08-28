394d852afad46fa6f26bea5f8ee3909183596c79
Meliza Caguimbal <meca@wpdomain.local>
2018-08-28 22:00:50 +0800

    create controllers

---
+++ Controllers/bookController.js
@@ -0,0 +1,25 @@
+var bookController = function(Book) {
+  var post = function(req, res) {
+    var book = new Book(req.body);
+    book.save();
+    res.status(201).send(book);
+  };
+
+  var get = function(req, res) {
+    var query = {};
+    if (req.query.genre) {
+      query.genre = req.query.genre;
+    }
+    Book.find(query, function(err, books) {
+      if (err) res.status(500).send(err);
+      else res.json(books);
+    });
+  };
+
+  return {
+    get: get,
+    post: post,
+  };
+};
+
+module.exports = bookController;
--- Routes/bookRoutes.js
+++ Routes/bookRoutes.js
@@ -2,24 +2,10 @@ var express = require('express');
 
 var routes = function(Book) {
   var bookRouter = express.Router();
-
-  bookRouter
-    .route('/Books')
-    .post(function(req, res) {
-      var book = new Book(req.body);
-      book.save();
-      res.status(201).send(book);
-    })
-    .get(function(req, res) {
-      var query = {};
-      if (req.query.genre) {
-        query.genre = req.query.genre;
-      }
-      Book.find(query, function(err, books) {
-        if (err) res.status(500).send(err);
-        else res.json(books);
-      });
-    });
+  var bookController = require('../Controllers/bookController')(Book);
+  bookRouter.route('/')
+    .post(bookController.post)
+    .get(bookController.get)
 
   bookRouter.use('/:bookId', function(req, res, next) {
     Book.findById(req.params.bookId, function(err, book) {
